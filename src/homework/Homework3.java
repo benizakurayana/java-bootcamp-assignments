64b953def75287159586f31f3069b2b84b428d86
benizakurayana <122624742+benizakurayana@users.noreply.github.com>
2023-07-11 03:59:47 +0800

    Updated Homework3.

--- src/homework/Homework3.java
+++ src/homework/Homework3.java
@@ -1,77 +1,84 @@
 package homework;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Homework3 {
-	
+
 	static Scanner sc = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		Homework3 hw3 = new Homework3();
 
-//		hw3.distinguishTriangle();
-//		hw3.guessNumber();
+		hw3.distinguishTriangle();
+		hw3.guessNumber();
 		hw3.lotteryPickNum();
 		sc.close();
 	}
+
 	// Q1
 	public void distinguishTriangle() {
-//		Scanner sc = new Scanner(System.in);
+
 		int[] triangle = new int[3];
 		String type = "";
-		boolean isOther = true;
 		boolean vertical = false;
-		
+
 		System.out.println("請輸入三個整數：");
 		for (int i = 0; i < 3; i++) {
 			triangle[i] = sc.nextInt();
 		}
+
 		int a = 0, b = 0, c = 0;
-		for (int i = 0; i < 6; i++) {
+		for (int i = 0; i < 3; i++) {
+			// To ensure each element in the array is compared with all the others.
 			a = triangle[i % 3];
 			b = triangle[(i + 1) % 3];
 			c = triangle[(i + 2) % 3];
-			
-			if (a == 0 || b == 0 || c == 0) {
+
+			// Check triangle type: not a triangle, equilateral(正三角形) or isosceles(等腰三角形)
+			// If it's none of the above, then it's an "other" type.
+			if (a == 0 || a + b <= c || Math.abs(a - b) >= c) {
 				type = "不是";
-				isOther = false;
 				break;
-			} else if (a == b || a == c) {
+			} else if (a == b) {   
+				// Definition of an isosceles triangle: it has two sides of equal length.
+				// It's notable that an equilateral triangle is theoretically also a special case of an isosceles one.
+				// Therefore, we shall place the condition of the former before the latter.
 				if (a == b && a == c) {
 					type = "正";
-					isOther = false;
 					break;
-				} else {
-					type = "等腰";
-					isOther = false;
-					break;
-				}
-				
-			} 
-			if (a * a + b * b == c * c && a != 1) {
+				} 
+				type = "等腰";
+				break;
+			}
+			
+			// Check if it's a right triangle(直角三角形) by Pythagorean theorem(畢氏定理)
+			if (a * a + b * b == c * c) {
 				vertical = true;
 			}
 		}
-		
-		if (isOther) {
+		// If none of the conditions above is fulfilled, the type String would remain empty.
+		if (type.isEmpty()) {
 			type = "其他";
 		}
-		
+
 		if (vertical) {
 			System.out.println(type + "直角三角形");
 		} else {
 			System.out.println(type + "三角形");
 		}
-		
 	}
-	
+
 	// Q2
 	public void guessNumber() {
-		int answer = (int)(Math.random() * 101);
-//		Scanner sc = new Scanner(System.in);
-		System.out.println("開始猜數字吧！(0~100)");
+		int answer = (int) (Math.random() * 101);
+
+		System.out.println("開始猜數字吧！(0~100)。不想猜了就輸入-1可離開。");
 		int guess = sc.nextInt();
 		while (true) {
+			if (guess == -1) {
+				break;
+			}
 			if (guess == answer) {
 				System.out.println("猜對了，答案是" + answer);
 				break;
@@ -85,27 +92,62 @@ public class Homework3 {
 		}
 
 	}
-	
+
 	// Q3
 	public void lotteryPickNum() {
-		System.out.println("阿文...請輸入你討厭哪個數字？(1~9)");
+		System.out.println("阿文...請輸入樂透最高到幾號？(可輸入7~99)");
+		int highest = sc.nextInt();
+		System.out.println("阿文...請輸入你討厭哪個數字？(可輸入1~9)");
 		int unwanted = sc.nextInt();
-		int[] choices = new int[49];
-		for (int i = 0; i < 49 / 10; i++) {
-			for (int j = 0; j <= 9; j++) {
-				if (i == unwanted || j == unwanted || i == 0 && j == 0) {
-					continue;
-				} else {
-					int num = i * 10 + j;
-						choice += 1;
-						System.out.print(num + " ");
-						
-						}
-				}	
+
+		int index = highest - (int) (highest / 10) + 1;
+		if (unwanted <= (int) (highest / 10)) {
+			index -= (highest % 10) + 1;
+		}
+
+		int[] choices = new int[index];
+		index = 0;
+		for (int i = 0; i <= highest / 10; i++) {
+			int ones = 9;
+			if (i == highest / 10) {
+				ones = highest % 10;
+			}
+			for (int j = 0; j <= ones; j++) {
+				if (i != unwanted && j != unwanted && (i != 0 || j != 0)) {
+					choices[index] = i * 10 + j;
+					index++;
+				}
 			}
 		}
-		System.out.println(" " + "總共有" + choice +"個數字可選" );
+		for (index = 0; index < choices.length; index++) {
+			if (choices[index] != 0) {
+				System.out.print(choices[index] + "\t");
+				if (index == 5 || (index + 1) % 6 == 0) {
+					System.out.println();
+				}
+			} else {
+				System.out.printf("總共有%d個數字可選\n", index);
+				break;
+			}
+		}
+
+		int[] randomIndices = new int[6];
+		for (int i = 0; i < 6; i++) {
+			randomIndices[i] = (int) (Math.random() * (index));
+			for (int j = 0; j < i; j++) {
+				while (randomIndices[i] == randomIndices[j]) {
+					randomIndices[i] = (int) (Math.random() * (index));
+					j = 0;
+				}
+			}
+		}
+
+		System.out.println("幫你隨機選6個 ^_^");
+		Arrays.sort(randomIndices);
+		for (int randomIndex : randomIndices) {
+			System.out.print(choices[randomIndex] + "\t");
+		}
+
 	}
-	
+
 }
-	
