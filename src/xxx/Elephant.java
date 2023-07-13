0f7918b4a69b79be95b9f8c3f348a53207a8986d
Jane <122624742+benizakurayana@users.noreply.github.com>
2023-07-13 17:14:31 +0800

    Added and updated lecture practices.

--- src/xxx/Elephant.java
+++ src/xxx/Elephant.java
@@ -3,15 +3,29 @@ package xxx;
 public class Elephant extends Animal{
 	String name;
 	
+	public Elephant() {
+		
+	}
+	
 	public Elephant(int age, float weight, String name) {
 		super(age, weight);
 		this.name = name;
 	}
 	
+	@Override  // @ Annotation(標註)：給開發工具(Java compiler)或框架(framework)看的資訊
+			   // 請compiler用override的規則去檢查此method，以免被它誤認為是overloading而沒有報錯 (安全)
 	public void speak() {
 		super.speak();
 		System.out.printf("Name: %s\n", name);
 	}
+	
+//      編譯成功, 但不符合override規則
+//      因為被編譯器當成overloading, 1個是speak() (Animal) 1個是speak(int data) (Elephant)
+//		public void speak(int data) {
+//			super.speak();
+//			System.out.println("Name is = " + name);
+//		}
+	
 	
 	public static void main(String[] args) {
 		Animal ani1 = new Animal(3, 8.0f);
